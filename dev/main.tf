b3f9507fe4f2b19ec99209281f5d797719abf844
spartzames <spartzames@gmail.com>
2023-03-24 16:27:33 +0900

    mod main

--- dev/main.tf
+++ dev/main.tf
@@ -2,7 +2,32 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_vpc" "tfc_kthong_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "tfc_kthong_vpc"
+  }
+}
+
+resource "aws_subnet" "tfc_kthong_subnet_A" {
+  vpc_id = aws_vpc.tfc_kthong_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "tfc_kthong_subnet_A"
+  }
+}
+
+resource "aws_subnet" "tfc_kthong_subnet_B" {
+  vpc_id = aws_vpc.tfc_kthong_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2b"
+  tags = {
+    "Name" = "tfc_kthong_subnet_B"
+  }
+}
+
+resource "aws_instance" "tfc_kthong_ec2" {
   ami           = var.ami
   instance_type = "t2.micro"
 }
