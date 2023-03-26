ef42651e795dbb393987036e63ffbe9c8b57809f
spartzames <spartzames@gmail.com>
2023-03-27 08:47:17 +0900

    add security group

--- dev/main.tf
+++ dev/main.tf
@@ -39,6 +39,26 @@ resource "aws_subnet" "tfc_kthong_subnet_B" {
   }
 }
 
+resource "aws_security_group" "tfc_kthong_sec_grp" {
+  name        = "tfc_kthong_sec_grp"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_instance" "tfc_kthong_ec2" {
   ami           = var.ami
   instance_type = "t2.micro"
