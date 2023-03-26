7a78afa3f69cd99f9ffd82fb35cc9e21e172ad0e
spartzames <spartzames@gmail.com>
2023-03-27 08:44:06 +0900

    mod

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
+  profile = "default"
   region = var.region
 }
 
@@ -20,7 +32,7 @@ resource "aws_subnet" "tfc_kthong_subnet_A" {
 
 resource "aws_subnet" "tfc_kthong_subnet_B" {
   vpc_id = aws_vpc.tfc_kthong_vpc.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.10.0/24"
   availability_zone = "ap-northeast-2b"
   tags = {
     "Name" = "tfc_kthong_subnet_B"
@@ -30,4 +42,7 @@ resource "aws_subnet" "tfc_kthong_subnet_B" {
 resource "aws_instance" "tfc_kthong_ec2" {
   ami           = var.ami
   instance_type = "t2.micro"
+    tags = {
+    Name = "tfc_kthong_ec2"
+  }
 }
