8b87a4004f100ab554560e7781a1f8e065cc6951
spartzames <spartzames@gmail.com>
2023-03-27 09:53:25 +0900

    bug fix

--- dev/main.tf
+++ dev/main.tf
@@ -41,7 +41,7 @@ resource "aws_subnet" "tfc_kthong_subnet_B" {
 
 resource "aws_security_group" "tfc_kthong_sec_grp" {
   name        = "tfc_kthong_sec_grp"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.tfc_kthong_vpc.id}"
 
   ingress {
     from_port        = 80
@@ -63,7 +63,7 @@ resource "aws_security_group" "tfc_kthong_sec_grp" {
 resource "aws_instance" "tfc_kthong_ec2" {
   ami           = var.ami
   instance_type = "t2.micro"
-  vpc_security_group_ids      = ["${aws_security_group.bastion-sg.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.tfc_kthong_sec_grp.id}"]
     tags = {
     Name = "tfc_kthong_ec2"
   }
